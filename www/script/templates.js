a45bb278b4e964bc6ca947b03cbe8c2f5580d083
Patrick Cuba <cubap@slu.edu>
2020-12-03 14:57:28 -0600

    header/footer
    closes #6

---
+++ www/script/templates.js
@@ -0,0 +1,53 @@
+class NlHeader extends HTMLElement {
+    constructor() {
+        super()
+
+        this.attachShadow({ mode: 'open' })
+        this.shadowRoot.innerHTML = `
+<link rel="stylesheet" href="./css/style.css">
+<nav class="container">
+    <a href="../"> 🏠 </a> 
+    <a href="manuscripts.html"> manuscripts </a> 
+    <a href="maps.html"> maps </a> 
+    <a href="calligraphy.html"> calligraphy books </a>
+</nav>
+        `
+    }
+}
+
+customElements.define('nl-header', NlHeader)
+
+document.querySelector("header").replaceWith(document.createElement("nl-header"))
+
+class NlFooter extends HTMLElement {
+    constructor() {
+        super()
+
+        this.attachShadow({ mode: 'open' })
+        this.shadowRoot.innerHTML = `
+<link rel="stylesheet" href="./css/style.css">
+<div class="row container">
+    <div class="col-8">
+        <h2> Partners </h2>
+            <div class="row">
+                <a class="col" href="http://www.newberry.org/" target="_blank"><img alt="Newberry" src="//content.library.utoronto.ca/common/dh-images/paleography/NewberryLogo_with_tag.png"></a>
+                <a class="col" href="http://onesearch.library.utoronto.ca" target="_blank"><img alt="University of Toronto Libraries" src="//content.library.utoronto.ca/common/dh-images/paleography/UTL-logo.png"></a>
+            </div>
+            <div class="row">
+                <a class="col" href="http://lib.slu.edu/" style="line-height: 23.1111px;" target="_blank"><img alt="St. Louis University" src="//content.library.utoronto.ca/common/dh-images/paleography/slu.gif" style="width: 346px;"></a>
+                <a class="col" href="http://www.itergateway.org/" target="_blank"><img alt="Iter" src="//content.library.utoronto.ca/common/dh-images/paleography/iter-logo-new.png"></a>
+            </div>
+    </div>
+    <div class="col">
+        <h2 class="pane-title"> Contact </h2>
+        <p><a href="/contact">Contact</a><br><a href="/content/about-team">About the Team</a><br><a href="/contact">Terms of Use</a></p>
+        <p>Supported by a grant from The Andrew W. Mellon Foundation</p>
+        <p><a href="http://creativecommons.org/licenses/by-nc-nd/4.0/" rel="license"><img alt="Creative Commons Licence" src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png" style="border-width:0"></a><br><a href="http://paleography.library.utoronto.ca/">http://paleography.library.utoronto.ca/</a> is licensed under a <a href="http://creativecommons.org/licenses/by-nc-nd/4.0/" rel="license">Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License</a>.</p>
+    </div>
+</div> `
+    }
+}
+
+customElements.define('nl-footer', NlFooter)
+
+document.querySelector("footer").replaceWith(document.createElement("nl-footer"))

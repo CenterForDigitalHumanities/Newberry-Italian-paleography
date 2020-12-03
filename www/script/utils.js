b53621a3f74cbab13f9c9991463333f2f3324269
Patrick Cuba <cubap@slu.edu>
2020-12-03 00:16:00 -0600

    Fr => It

--- www/script/utils.js
+++ www/script/utils.js
@@ -1,3 +1,4 @@
+var records = document.querySelectorAll(".record")
 function renderManuscripts(elem, manuscripts) {
 
     const TPEN_BASE = "http://paleo.rerum.io/TPEN-NL/"
@@ -6,39 +7,66 @@ function renderManuscripts(elem, manuscripts) {
         "Date Issued", "Author(s) or Contributor(s)"
         // script, decoration, physical description
     ]
+    const FILTERS = {
+        "Type of Resource": "resource-type", Genre: "genre", Language: "language",
+        Script: "script", "Reading Difficulty": "readability", Topic: "topic",
+        Region: "region", "Time Period": "period", Repository: "repository"
+    }
+    const SEARCH = [
+        "Title", "Physical Description", "Note(s)", "Region",
+        "Topic", "Subject", "Repository", "Call Number", "Is Part Of"
+    ]
 
     let list = manuscripts.reduce((a, b) => a += `<div class="record" data-id="${b['@id']}">
     <h4>${b.label}</h4>
-        <div class="row">
-            <img class="thumbnail" src="http://placekitten.com/65/80">
-            <dl>
-            </dl>
-        </div>
-        <div class="btn-group">
-        <a href="./record.html?id=${b['@id']}">View</a> <a href="${TPEN_BASE}?projectID=${b.tpenProject}">Transcribe</a>
-        </div>
+    <div class="row">
+    <img class="thumbnail" src="http://placekitten.com/65/80">
+    <dl>
+    </dl>
+    </div>
+    <div class="btn-group">
+    <a href="./record.html?id=${b['@id']}">View</a> <a href="${TPEN_BASE}?projectID=${b.tpenProject}">Transcribe</a>
+    </div>
     </div>`, ``)
 
     elem.innerHTML = list
 
-    const records = Array.from(document.querySelectorAll(".record"))
-    records.forEach(r=>{
-        const url=r.getAttribute("data-id")
-        let dl=``
-        fetch(url)
-        .then(status => { if (!status.ok) { throw Error(status) } return status })
-        .then(response => response.json())
-        .then(manifest=> {
-            manifest.metadata.forEach(dat=>{
-                if(FIELDS.includes(dat.label)) {
-                    dl+=`<dt>${dat.label}</dt><dd>${Array.isArray(dat.value)?dat.value.join(", "):dat.value}</dd>`
-                }
+    records = document.querySelectorAll(".record")
+    let facets = {}
+    let loading = []
+    Array.from(records).forEach(r => {
+        const url = r.getAttribute("data-id")
+        let dl = ``
+        loading.push(fetch(url)
+            .then(status => { if (!status.ok) { throw Error(status) } return status })
+            .then(response => response.json())
+            .then(manifest => {
+                let metadataMap = new Map()
+                manifest.metadata.forEach(dat => {
+                    metadataMap.set(dat.label, Array.isArray(dat.value) ? dat.value.join(", ") : dat.value)
+                    if (FIELDS.includes(dat.label)) {
+                        dl += `<dt>${dat.label}</dt><dd>${metadataMap.get(dat.label)}</dd>`
+                    }
+                    if (FILTERS[dat.label]) {
+                        r.setAttribute("data-" + FILTERS[dat.label], metadataMap.get(dat.label))
+                        let values = (Array.isArray(dat.value)) ? dat.value : [dat.value]
+                        if (!facets[FILTERS[dat.label]]) {
+                            facets[FILTERS[dat.label]] = new Set()
+                        }
+                        for (const v of values) {
+                            facets[FILTERS[dat.label]] = facets[FILTERS[dat.label]].add(v.replace(/\?/g, ""))
+                        }
+                    }
+                })
+                r.setAttribute("data-query", SEARCH.reduce((a, b) => a += (metadataMap.has(b) ? metadataMap.get(b) : "*") + " ", ""))
+                r.querySelector("dl").innerHTML = dl
+                r.querySelector("img").src = manifest.sequences[0].canvases[0].thumbnail['@id']
             })
-            r.querySelector("dl").innerHTML = dl
-            r.querySelector("img").src = manifest.sequences[0].canvases[0].thumbnail['@id']
-        })
-        .catch(err => { throw Error(err) })
+            .catch(err => { throw Error(err) })
+        )
     })
+    Promise.all(loading).then(() => populateSidebar(facets, FILTERS))
+    query.addEventListener("input", filterQuery)
 }
 
 function fetchList(url) {
@@ -68,4 +96,69 @@ async function loadManuscripts(url, elem, pagination = [0,]) {
     }
     const manuscripts = (data.manifests || []).slice(pagination[0], pagination[1])
     renderManuscripts(elem, manuscripts)
+}
+
+queryReset.addEventListener("click", ev => {
+    Array.from(document.querySelectorAll(".clicked")).forEach(el => el.dispatchEvent(new Event("click")))
+    query.value = ""
+    query.dispatchEvent(new Event("input"))
+})
+
+function filterQuery(event) {
+    const queryString = event.target.value
+    Array.from(records).forEach(r => new RegExp(queryString, "i").test(r.getAttribute("data-query")) ? r.classList.remove("hide-query") : r.classList.add("hide-query"))
+    //Manuscripts.querySelectorAll(".record:not([data-query*='"+queryString+"'])")
+    updateCount()
+}
+function filterFacets(event) {
+    const clicked = event.target
+    clicked.classList.toggle("clicked")
+    const action = clicked.classList.contains("clicked") ? "add" : "remove"
+    const k = clicked.getAttribute("data-facet")
+    const v = clicked.textContent
+    Array.from(records).forEach(r => { if (!new RegExp(v, "i").test(r.getAttribute("data-" + k))) r.classList[action]("hide-facet") })
+    updateCount()
+}
+let progress;
+function updateCount() {
+    // let visibleCount = Array.from(records).filter(el=>el.offsetParent === null).length
+    let hiddenCount = document.querySelectorAll(".record[class*='hide-']").length
+    let countBar = query.previousElementSibling
+    let countBarValue = records.length - hiddenCount
+    countBar.max = records.length
+    countBar.textContent = countBarValue + " of " + countBar.max
+    clearInterval(progress)
+    let step = parseInt((countBarValue - countBar.value) / 25)
+    progress = setInterval(() => {
+        countBar.value += step
+        if (Math.abs(countBar.value - countBarValue) < 2) {
+            countBar.value = countBarValue
+            clearInterval(progress)
+        }
+    }, 10)
+    let facetsElements = document.querySelectorAll("facet")
+    Array.from(facetsElements).forEach(f => {
+        const k = f.getAttribute("data-facet")
+        const v = f.textContent
+        let count = document.querySelectorAll(".record:not([class*='hide-'])[data-" + k + "*='" + v + "']").length
+        f.setAttribute("data-count", count)
+        if (count === 0) {
+            f.classList.add("hide-sidebar")
+        } else {
+            f.classList.remove("hide-sidebar")
+        }
+    })
+}
+
+function populateSidebar(facets, FILTERS) {
+    let side = `<ul>`
+    for (const f in FILTERS) {
+        side += `<li>${f}</li>`
+        side += Array.from(facets[FILTERS[f]]).reduce((a, b) => a += `<facet data-facet="${FILTERS[f]}">${b}</facet>`, ``)
+    }
+    side += `</ul>`
+    facetFilter.innerHTML = side
+    let facetsElements = document.querySelectorAll("[data-facet]")
+    Array.from(facetsElements).forEach(el => el.addEventListener("click", filterFacets))
+    updateCount()
 }

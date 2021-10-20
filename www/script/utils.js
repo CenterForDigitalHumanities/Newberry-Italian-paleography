a01512542c5945bf9f82c84fe5f6d0d0efcc6106
Patrick Cuba <cubap@slu.edu>
2021-10-20 10:45:57 -0500

    Update utils.js

--- www/script/utils.js
+++ www/script/utils.js
@@ -164,8 +164,21 @@ function populateSidebar(facets, FILTERS) {
     let facetsElements = document.querySelectorAll("[data-facet]")
     Array.from(facetsElements).forEach(el => el.addEventListener("click", filterFacets))
     updateCount()
+    loadQuery()
 }
 
+const loadQuery = () => {
+    const params = new URLSearchParams(location.search)
+    params.forEach((v,k)=>{
+        document.querySelectorAll(`[data-facet="${k}"][data-label="${v}"]`)
+         .forEach(el=>el.click())
+    })
+    const queryString = params.get("q")
+    if(queryString){
+        query.value = queryString
+        query.dispatchEvent(new Event('input'))
+    }
+}
 let templates = document.createElement("script")
 templates.src = "script/templates.js"
 document.body.append(templates)
